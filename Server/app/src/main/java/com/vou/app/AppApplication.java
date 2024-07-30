abe76efea5e5b0c7fef07fbbab81d0a40cf1e6fe
BaOfBu <111734920+BaOfBu@users.noreply.github.com>
2024-07-30 09:04:41 +0700

    initial

---
+++ Server/app/src/main/java/com/vou/app/AppApplication.java
@@ -0,0 +1,13 @@
+package com.vou.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
