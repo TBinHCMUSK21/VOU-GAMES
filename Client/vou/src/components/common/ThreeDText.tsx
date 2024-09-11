cff4bb571dc2ce3dcb38c6906e28ff1b7b6ab0be
BaOfBu <111734920+BaOfBu@users.noreply.github.com>
2024-09-11 10:37:54 +0700

    all done except voucher

--- Client/vou/src/components/common/ThreeDText.tsx
+++ Client/vou/src/components/common/ThreeDText.tsx
@@ -9,9 +9,9 @@ interface ThreeDTextProps {
 const ThreeDText: React.FC<ThreeDTextProps> = ({ text, color = 'text-gray-800', size = 'text-4xl' }) => {
   return (
     <div className="relative inline-block">
-      <span className={`absolute top-1 left-1 ${size} font-bold ${color} opacity-70`}>{text}</span>
+      <span className={`absolute top-[4px] left-[4px] ${size} font-bold ${color} opacity-70`}>{text}</span>
       {/* <span className={`absolute top-2 left-2 ${size} font-bold ${color} opacity-50`}>{text}</span> */}
-      <span className={`absolute top-2 left-2 ${size} font-bold ${color} opacity-30`}>{text}</span>
+      <span className={`absolute top-[2px] left-[2px] ${size} font-bold ${color} opacity-50`}>{text}</span>
       <span className={`relative ${size} font-bold ${color}`}>{text}</span>
     </div>
   );
